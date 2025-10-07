b5db32a20c67c271e58abc85a64b6699e931b591
Tomer Friling <tomer.friling@weizmann.ac.il>
2025-10-07 21:09:14 +0300

    add all functionallity of one rumor: catch counter the number of catches to finish curren rumour, generate task: get a new task, xp_calculator: evaluates the xp from catches + bonus

---
+++ hunter_rumours_master_with_block/single_rumour.py
@@ -0,0 +1,24 @@
+from data_tables import complete_probability, xp_table
+import random
+
+
+def catch_counter(task: str):
+    counter = 0
+    while True:
+        counter += 1
+        if random.randint(1, complete_probability[task]) == 1:
+            return counter
+
+
+def generate_task(previous_task: str, chin_is_blocked=False):
+    next_task = ["Herbiboar", "dashing_kebbit"]
+    if not chin_is_blocked:
+        next_task = ["Herbiboar", "dashing_kebbit"] + ["red_chinchompa"]
+
+    if previous_task == "tecu_salamander":
+        return random.choice(next_task)
+    return random.choice(next_task + ["tecu_salamander"])
+
+
+def xp_calculator(catches: int, task: str):
+    return catches * xp_table["task"] + xp_table["master_rumour"]
