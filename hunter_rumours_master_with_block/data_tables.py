323c238b0cc4734079e1c3bb1dc1285a3e660a82
Tomer Friling <tomer.friling@weizmann.ac.il>
2025-10-07 17:57:42 +0300

    For the master rumour simulator add table script that hold data abou xp per catch and probability to finish a rumour

---
+++ hunter_rumours_master_with_block/data_tables.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+xp_table = {
+    "red_chincompa": 265,
+    "Herbiboar": 2461,
+    "tecu_salamander": 344,
+    "dashing_kebbit": 156,
+    "master_rumour": 6240
+}
+
+complete_probability = {
+    "red_chincompa": 50,
+    "Herbiboar": 7,
+    "tecu_salamander": 25,
+    "dashing_kebbit": 10
+}
